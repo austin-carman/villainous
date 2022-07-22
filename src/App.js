b3ee8ac5295787503a10393ec930478a714c7525
Austin Carman <abcarman12@gmail.com>
2022-07-21 17:04:04 -1000

    start turn screen for player confirmation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Player1 from './components/Player1';
+import StartTurn from './components/StartTurn';
 
 function App() {
   return (
     <div className="App">
-      <Player1 />
+      {/* <Player1 /> */}
+      <StartTurn />
     </div>
   );
 }
